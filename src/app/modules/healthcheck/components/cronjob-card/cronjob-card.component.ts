01b9dea3dab47281c3c4a4ffac3fd3c2144dcda0
Slavomir Mazur <slavomir.mazur@pantheon.tech>
2020-09-03 08:35:31 +0200

    Cronjobs status added to healthcheck tab
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

---
+++ src/app/modules/healthcheck/components/cronjob-card/cronjob-card.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HealthcheckService } from '../../healthcheck.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorMessageDialogComponent } from '../../dialogs/error-message-dialog/error-message-dialog.component';
+
+@Component({
+  selector: 'app-cronjob-card',
+  templateUrl: './cronjob-card.component.html',
+  styleUrls: ['./cronjob-card.component.scss']
+})
+export class CronjobCardComponent implements OnInit {
+  @Input() cronjobName: string;
+  @Input() cronjobData: any;
+  statusColor = '#cdcdcd';
+  status = '';
+  errorMessage = '';
+  startDate: Date;
+  endDate: Date;
+  lastSuccessfullDate: Date;
+  isLoading = false;
+  constructor(public dialog: MatDialog, private healthcheckService: HealthcheckService) { }
+
+  ngOnInit(): void {
+    this.status = this.cronjobData.status;
+    this.startDate = new Date(this.cronjobData.start * 1000);
+    this.endDate = new Date(this.cronjobData.end * 1000);
+    this.lastSuccessfullDate = new Date(this.cronjobData['last_successfull'] * 1000);
+    this.errorMessage = this.cronjobData.error;
+    this.statusColor = this.healthcheckService.getColorByStatus(this.status);
+  }
+
+  onInfoClick() {
+    const dialogRef = this.dialog.open(ErrorMessageDialogComponent, {
+      data: {
+        errorMessage: this.errorMessage
+      }
+    });
+  }
+}
