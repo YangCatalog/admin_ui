583e215be269b0b3a66830746c8e1ff609b96d66
Slavomir Mazur <slavomir.mazur@pantheon.tech>
2021-09-22 12:32:02 +0200

    Add motivation and registration date to the users tables
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- src/app/modules/mysql-management/pages/mysql/mysql.headers.ts
+++ src/app/modules/mysql-management/pages/mysql/mysql.headers.ts
@@ -5,7 +5,8 @@ const UsersTableHeaders = [
   { label: 'Email', value: 'email' },
   { label: 'Models Provider', value: 'models-provider', hide: true },
   { label: 'Access Rights SDO', value: 'access-rights-sdo' },
-  { label: 'Access Rights Vendor', value: 'access-rights-vendor' }
+  { label: 'Access Rights Vendor', value: 'access-rights-vendor' },
+  { label: 'Registration Date', value: 'registration-datetime' }
 ];
 
 const UsersTempTableHeaders = [
@@ -13,7 +14,9 @@ const UsersTempTableHeaders = [
   { label: 'Last Name', value: 'last-name' },
   { label: 'Username', value: 'username' },
   { label: 'Email', value: 'email' },
-  { label: 'Models Provider', value: 'models-provider', hide: true }
+  { label: 'Models Provider', value: 'models-provider', hide: true },
+  { label: 'Registration Date', value: 'registration-datetime' },
+  { label: 'Motivation', value: 'motivation' }
 ];
 
 export { UsersTableHeaders, UsersTempTableHeaders };
